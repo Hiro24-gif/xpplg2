f4f1b2bf9d354d01ce4c491f39a1515d85b47d92
Hiro <114798397+Hiro24-gif@users.noreply.github.com>
2022-11-29 15:16:07 +0700

    Add files via upload

---
+++ ganjilgenap.js
@@ -0,0 +1,9 @@
+var wow = require('readline-sync');
+var a = wow.question(' masukan bilangan ')
+if (a=isNaN) 
+console.log('input yang anda masukan bukan angka ')
+else{
+if (a %2)
+{console.log('bilangan yang anda masukan adalah bilangan ganjil')}
+else{console.log('bilangan yang anda masukan adalah bilangan genap')}}
+
